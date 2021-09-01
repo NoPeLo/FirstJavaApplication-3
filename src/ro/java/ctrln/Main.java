7990ac573b27c319c1ac952ae6394c65a4fd49cc
NoPeLo <narcis2000gg@gmail.com>
2021-09-01 16:38:16 +0300

    test

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,7 +1,7 @@
 package ro.java.ctrln;
 
 public class Main {
-
+        // test comment
     public static void main(String[] args) {
 	    System.out.println("I learn the java language and about JVM!");
     }
